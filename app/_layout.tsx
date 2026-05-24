891f16158221313b3c4b136c2d68ff11c486b2c4
Nazil1712 <ndhalwala@gmail.com>
2026-05-24 12:31:57 +0530

    TextInput, TouchableOpacity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,38 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView>
+      <View style={{ padding: 16 }}>
+        <Text>Hello World</Text>
+
+        <TextInput
+          placeholder="Search City"
+          placeholderTextColor={"#999"}
+          style={{
+            borderWidth: 1,
+            borderColor: "#ddd",
+            borderRadius: 8,
+            padding: 10,
+            marginTop: 12,
+          }}
+        />
+
+        <TouchableOpacity
+          onPress={() => alert("Searching...")}
+          style={{
+            marginTop: 12,
+            backgroundColor: "#8caaea",
+            padding: 12,
+            borderRadius: 8,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Search</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
