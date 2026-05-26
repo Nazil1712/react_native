56de1f2a20cb9d3e002da47617c91b8eaabe3abd
Nazil1712 <ndhalwala@gmail.com>
2026-05-26 15:56:59 +0530

    Sign u[ page created

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,217 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useAuth, useSignUp } from "@clerk/expo";
+import { Link, useRouter } from "expo-router";
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function SignUp() {
+  const { signUp, errors, fetchStatus } = useSignUp();
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [code, setCode] = useState("");
+
+  const isLoading = fetchStatus == "fetching";
+
+  if (signUp.status == "complete" || isSignedIn) {
+    return null;
+  }
+
+  const onSignUpPress = async () => {
+    const { error } = await signUp.password({
+      emailAddress: email,
+      password,
+      firstName,
+      lastName,
+    });
+
+    if (error) {
+      // console.log("Error", error.message);
+      // console.error(JSON.stringify(error,null,2))
+      alert(error.message);
+      return;
+    }
+
+    if (!error) await signUp.verifications.sendEmailCode();
+  };
+
+  const onVerifyPress = async () => {
+    await signUp.verifications.verifyEmailCode({
+      code,
+    });
+
+    if (signUp.status == "complete") {
+      await signUp.finalize({
+        navigate: ({ decorateUrl }) => {
+          const url = decorateUrl("/");
+          router.replace(url as any);
+        },
+      });
+    }
+  };
+
+  if (
+    signUp.status == "missing_requirements" &&
+    signUp.unverifiedFields.includes("email_address") &&
+    signUp.missingFields.length == 0
+  ) {
+    return (
+      <View className="flex-1 justify-center px-6 py-12">
+        <View className="w-full flex-row justify-center">
+          <Image
+            source={require("../../assets/images/kribb.png")}
+            style={{ width: 100, height: 100 }}
+            className="mb-8"
+            resizeMode="contain"
+          />
+        </View>
+
+        <Text className="text-3xl font-bold text-gray-800 mb-2">
+          Verify your account
+        </Text>
+        <Text className="text-gray-500 mb-8">We sent a code to {email}</Text>
+
+        <View className="flex gap-6 mb-4">
+          <TextInput
+            className="flex-1 border border-gray-300 rounded-xl px-4 py-3"
+            placeholder="Enter verification code"
+            placeholderTextColor={"#9CA3AF"}
+            keyboardType="number-pad"
+            value={code}
+            onChangeText={setCode}
+          />
+          {errors.fields.code && (
+            <Text className="text-red-500 mb-4">
+              {errors.fields.code.message}
+            </Text>
+          )}
+
+          <TouchableOpacity
+            onPress={onVerifyPress}
+            disabled={isLoading}
+            className="w-full bg-blue-600 py-4 items-center mb-4 rounded-xl text-white"
+          >
+            {isLoading ? (
+              <ActivityIndicator color={"white"} />
+            ) : (
+              <Text className="text-white text-base font-bold">Verify</Text>
+            )}
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="py-2"
+            onPress={() => signUp.verifications.sendEmailCode()}
+          >
+            <Text className="text-blue-600">I need a new code</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">SignUp</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      className="bg-white"
+      keyboardShouldPersistTaps="handled"
+    >
+      <View className="flex-1 justify-center px-6 py-12">
+        <View className="w-full flex-row justify-center">
+          <Image
+            source={require("../../assets/images/kribb.png")}
+            style={{ width: 100, height: 100 }}
+            className="mb-8"
+            resizeMode="contain"
+          />
+        </View>
+
+        <Text className="text-3xl font-bold text-gray-800 mb-2">
+          Create account
+        </Text>
+        <Text className="text-gray-500 mb-8">Find your dream home today</Text>
+
+        <View className="flex flex-row gap-3 mb-4">
+          <TextInput
+            className="flex-1 border border-gray-300 rounded-xl px-4 py-3"
+            placeholder="First Name"
+            placeholderTextColor={"#9CA3AF"}
+            autoCapitalize="words"
+            value={firstName}
+            onChangeText={setFirstName}
+          ></TextInput>
+
+          <TextInput
+            className="flex-1 border border-gray-300 rounded-xl px-4 py-3"
+            placeholder="Last Name"
+            placeholderTextColor={"#9CA3AF"}
+            autoCapitalize="words"
+            value={lastName}
+            onChangeText={setLastName}
+          />
+        </View>
+
+        <TextInput
+          className="w-full border border-gray-300 rounded-xl px-4 py-3 mb-4"
+          placeholder="Email address"
+          placeholderTextColor={"#9CA3AF"}
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        {errors.fields.emailAddress && (
+          <Text className="text-red-500 mb-4">
+            {errors.fields.emailAddress.message}
+          </Text>
+        )}
+
+        <TextInput
+          className="w-full border border-gray-300 rounded-xl px-4 py-3 mb-4"
+          placeholder="password"
+          placeholderTextColor={"#9CA3AF"}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {errors.fields.password && (
+          <Text className="text-red-500 mb-4">
+            {errors.fields.password.message}
+          </Text>
+        )}
+
+        <TouchableOpacity
+          onPress={onSignUpPress}
+          disabled={isLoading}
+          className="w-full bg-blue-600 py-4 items-center mb-4 rounded-xl text-white"
+        >
+          {isLoading ? (
+            <ActivityIndicator color={"white"} />
+          ) : (
+            <Text className="text-white text-base font-bold">Sign Up</Text>
+          )}
+        </TouchableOpacity>
+
+        <View className="flex flex-row justify-center gap-2">
+          <Text className="text-gray-500">Already have an account?</Text>
+          <Link href={"/sign-in"}>
+            <Text className="text-blue-600 font-semibold">Sign In</Text>
+          </Link>
+        </View>
+
+        {/* Clerk want you to have this <View> for their captcha verification purpose */}
+        <View nativeID="clerk-captcha"></View>
+      </View>
+    </ScrollView>
   );
 }
